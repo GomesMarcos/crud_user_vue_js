225aae370e56adc5f0d8d6f15ccdf5f0bab0bff5
marcos figueiredo <marcos.figueiredo@montreal.com.br>
2021-01-07 16:01:51 -0300

    update components, logo, import statics

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <CrudUsers />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Nav from "./components/Nav.vue";
+import CrudUsers from "./components/CrudUsers.vue";
+import Usuario from './services/usuarios'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CrudUsers,
+  },
+  mounted() {
+    Usuario.listar().then(response => {
+      console.log('mounted')
+      console.log(response)
+    })
+  },
+};
+
 </script>
 
 <style>
@@ -24,5 +30,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo-wrapper img {
+  max-width: 64px;
 }
 </style>
---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="nav navbar navbar-expand-sm navbar-dark bg-dark justify-center">
+    <a class="nav-link position-relative p-0" href="#">
+      <img src="../assets/logo.png" alt="Logo do projeto" class="nav-brand" />
+    </a>
+    <h1 class="nav-title position-absolute text-center w-100 m-0">
+      CRUD Usando Vue, MySQL e Node
+    </h1>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+};
+</script>
+
+<style>
+.nav-link {
+  z-index: 1;
+}
+.nav-brand {
+  max-width: 3rem;
+}
+.nav-title {
+  color: white;
+}
+</style>
