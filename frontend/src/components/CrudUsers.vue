81a26730ff179419544ceef2fa3a0b4f28537e65
marcos figueiredo <marcos.figueiredo@montreal.com.br>
2021-01-07 16:26:36 -0300

    import node-sass and add responsive table

--- frontend/src/components/CrudUsers.vue
+++ frontend/src/components/CrudUsers.vue
@@ -1,5 +1,45 @@
 <template>
+<div>
   <Nav />
+  <div class="container">
+    <table class="table table-stripped table-hover nt-5">
+      <thead class="table-head">
+        <tr>
+          <th scope="col">Student Code </th>
+          <th scope="col">Percentage of marks</th>
+          <th scope="col">Grade</th>
+          <th scope="col">Remarks</th>
+        </tr>
+      </thead>
+      <tbody class="table-body">
+        <tr>
+          <td data-label="Student Code">S001</td>
+          <td data-label="percent">92</td>
+          <td data-label="grade">A+</td>
+          <td data-label="remakr">Excellent</td>
+        </tr>
+        <tr>
+          <td data-label="Student Code">S002</td>
+          <td data-label="percent">76</td>
+          <td data-label="grade">B+</td>
+          <td data-label="remakr">Good</td>
+        </tr>
+        <tr>
+          <td data-label="Student Code">S003</td>
+          <td data-label="percent">60</td>
+          <td data-label="grade">C+</td>
+          <td data-label="remakr">Regular</td>
+        </tr>
+        <tr>
+          <td data-label="Student Code">S004</td>
+          <td data-label="percent">45</td>
+          <td data-label="grade">E+</td>
+          <td data-label="remakr">Bad</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
 </template>
 
 <script>
@@ -10,28 +50,98 @@ export default {
   components: { Nav },
   name: "crud_users",
 
+  data() {
+    return {
+      usuarios: []
+    }
+  },
+
   mounted() {
     Usuarios.listar().then((response) => {
-      console.log(response.data);
-    });
+      this.usuarios = response.data;
+    })
+    .then( () => {
+      console.log('this.usuarios', this.usuarios);
+    })
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style lang="scss">
+
+$breakpoint: 768px;
+
+.table {
+  margin-bottom: 0 !important;
+
+  thead {
+    tr > th {
+      color: gray;
+      font-size: 11px;
+    }
+  }
+
+  tbody {
+    tr {
+      color: darkgray;
+      font-size: 9px;
+    
+      &:nth-of-type(odd) {
+        background-color: #eee;
+      }
+    }
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+td,
+th {
+  border: none !important; 
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+  .table-hover {
+
+    tbody {
+      tr:hover {
+        background-color: lighten(#343a40, 20%) !important;
+        color: lightgray !important;
+        box-shadow: 2px 2px 6px;
+        transition: linear;
+        transition-duration: 150ms;
+        transition-property: all;
+      }
+    }
+  }
+
+  @media all and (max-width: $breakpoint) {
+   table {
+    border: 0;
+    
+    thead {
+      display: none;
+    }
+
+    tr { 
+      display: flex;
+      flex-direction: column;
+      border-bottom: 1em solid darken(gray, 15%);
+     }
+
+    td {
+      display: block;
+      text-align: right;
+
+      &::before {
+        content: attr(data-label);
+        float: left;
+        text-transform: uppercase;
+        font-weight: 700;
+        // color: gray;
+
+        &:hover {
+          color: lightgray;
+        }
+      }
+    }
+   } 
+  }
 </style>
